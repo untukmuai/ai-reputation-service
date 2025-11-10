0a93601f4376e22221c3ccabf5e2a1093907671c
Ariq Fachry <ariq@untukmu.ai>
2025-11-11 06:47:56 +0700

    fix: persona request model

--- models/requests/persona_request.py
+++ models/requests/persona_request.py
@@ -2,7 +2,7 @@ from typing import Any, Dict, Optional, List
 from pydantic import BaseModel
 from robyn.types import Body
 
-class RequestSortingHat(Body):
+class RequestSortingHat(BaseModel, Body):
     digital_dna: List[str]
     old_persona: Optional[str]
     old_tier: Optional[int]
