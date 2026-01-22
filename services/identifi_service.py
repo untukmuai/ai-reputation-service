12a3b9d2ce1ecd55732c09bdf2d80788deeae961
Ariq Fachry <ariq@untukmu.ai>
2026-01-22 11:41:22 +0700

    remove: unused imports

--- services/identifi_service.py
+++ services/identifi_service.py
@@ -3,19 +3,14 @@ import math
 from statistics import mean
 from models.requests.identifi_request import RequestIdentifiScore, RequestIdentifiScoreV2
 from models.requests.tweet_request import Tweets
-from models.responses.base_response import BaseResponse, ErrorResponse
 from services.identifi_util_service import IdentifiScoreUtil
 from services.somnia_referral_service import SomniaReferralService
-import textstat
-import numpy as np
 import math
-from collections import Counter
 import re
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from sentence_transformers import SentenceTransformer, util
 import time
-import orjson
 import os
 
 os.environ['CUDA_VISIBLE_DEVICES'] = ''
@@ -45,82 +40,7 @@ class IdentifiScore:
     MEDIA_RICHNESS_WEIGHT = {"image": 0.3, "video": 0.6}
 
     SIMILARITY_SPAM_THRESHOLDS = 0.9
-    SPAM_PENALTY_MULTIPLIER = 10
-
-    @staticmethod
-    async def calculate_identifi_log(payload: RequestIdentifiScore):
-        try:
-            avg_views    = round(mean(i.views if i.views is not None else 0 for i in payload.tweets))
-            avg_likes    = round(mean(i.likes if i.likes is not None else 0 for i in payload.tweets))
-            avg_replies  = round(mean(i.replies if i.replies is not None else 0 for i in payload.tweets))
-            avg_retweets = round(mean(i.retweets if i.retweets is not None else 0 for i in payload.tweets))
-            badges_minted = payload.badges_minted
-            quest_completed = payload.quest_completed
-            referral_count = 0
-
-            if payload.address:
-                referral_service = SomniaReferralService()
-                referral_count = await referral_service.get_referral_count_async(payload.address)
-
-
-            #network/social score calc
-            follower_score = (math.log10(payload.public_metrics.followers_count + 1)) * 50
-            view_score =  (math.log10(avg_views + 1)) * 50
-            social_score = follower_score + view_score
-
-            #engagement score calc
-            total_engagement = avg_likes + avg_replies + avg_retweets
-            engagement_score = math.log10(total_engagement + 1) * 75
-
-            #on chain score calc
-            referral_score = math.log10(referral_count + 1) * 30
-            badges_score = payload.total_badges_reward * 0.4
-            onchain_score = badges_score + referral_score
-
-            #identifi final cacl
-            final_identifi_score = social_score + engagement_score + onchain_score
-
-
-            
-            # total_social_score = followers_count + avg_views
-            # total_reputation_score = avg_likes + avg_replies + avg_retweets
-            # log_social_score = math.log10(total_social_score + 1) * 50
-            # log_reputation_score = math.log10(total_reputation_score + 1) * 75
-            # log_onchain_score = math.log10(total_onchain_score + 1) * 50
-            # log_governance_score = math.log10(0 + 1) * 25
-
-            return {
-                "social_score": {
-                    "followers_count": payload.public_metrics.followers_count,
-                    "avg_views": avg_views,
-                    "final": social_score,
-                    # "log_social_score": log_social_score,
-                },
-                "reputation_score": {
-                    "avg_likes": avg_likes,
-                    "avg_replies": avg_replies,
-                    "avg_retweets": avg_retweets,
-                    "final": engagement_score,
-                    # "log_reputation_score": log_reputation_score,
-                },
-                "governance_score": {
-                    "final": 0,
-                    # "log_governance_score": log_governance_score,
-                },
-                "onchain_score": {
-                    "badges_minted": badges_minted,
-                    "quest_completed": quest_completed,
-                    "total_badges_reward": payload.total_badges_reward,
-                    "referral_count": referral_count,
-                    "final": onchain_score,
-                    # "log_onchain_score": total_onchain_score,
-                },
-                "identifi_score": final_identifi_score
-                # "log_identifi_score": log_social_score + log_reputation_score + total_onchain_score + log_governance_score
-            }
-        except Exception as e:
-            logger.exception("calculate_identifi_log_err: %s", e)
-            raise 
+    SPAM_PENALTY_MULTIPLIER = 8
 
     @staticmethod
     def get_media_richness_score(photos: int, videos: int):
@@ -479,3 +399,79 @@ class IdentifiScore:
         except Exception as e:
             logger.exception(f"IDENTIFI_SCORE_V2_ERR {payload.username} {e}")
             raise
+    
+    # deprecated function
+    @staticmethod
+    async def calculate_identifi_log(payload: RequestIdentifiScore):
+        try:
+            avg_views    = round(mean(i.views if i.views is not None else 0 for i in payload.tweets))
+            avg_likes    = round(mean(i.likes if i.likes is not None else 0 for i in payload.tweets))
+            avg_replies  = round(mean(i.replies if i.replies is not None else 0 for i in payload.tweets))
+            avg_retweets = round(mean(i.retweets if i.retweets is not None else 0 for i in payload.tweets))
+            badges_minted = payload.badges_minted
+            quest_completed = payload.quest_completed
+            referral_count = 0
+
+            if payload.address:
+                referral_service = SomniaReferralService()
+                referral_count = await referral_service.get_referral_count_async(payload.address)
+
+
+            #network/social score calc
+            follower_score = (math.log10(payload.public_metrics.followers_count + 1)) * 50
+            view_score =  (math.log10(avg_views + 1)) * 50
+            social_score = follower_score + view_score
+
+            #engagement score calc
+            total_engagement = avg_likes + avg_replies + avg_retweets
+            engagement_score = math.log10(total_engagement + 1) * 75
+
+            #on chain score calc
+            referral_score = math.log10(referral_count + 1) * 30
+            badges_score = payload.total_badges_reward * 0.4
+            onchain_score = badges_score + referral_score
+
+            #identifi final cacl
+            final_identifi_score = social_score + engagement_score + onchain_score
+
+
+            
+            # total_social_score = followers_count + avg_views
+            # total_reputation_score = avg_likes + avg_replies + avg_retweets
+            # log_social_score = math.log10(total_social_score + 1) * 50
+            # log_reputation_score = math.log10(total_reputation_score + 1) * 75
+            # log_onchain_score = math.log10(total_onchain_score + 1) * 50
+            # log_governance_score = math.log10(0 + 1) * 25
+
+            return {
+                "social_score": {
+                    "followers_count": payload.public_metrics.followers_count,
+                    "avg_views": avg_views,
+                    "final": social_score,
+                    # "log_social_score": log_social_score,
+                },
+                "reputation_score": {
+                    "avg_likes": avg_likes,
+                    "avg_replies": avg_replies,
+                    "avg_retweets": avg_retweets,
+                    "final": engagement_score,
+                    # "log_reputation_score": log_reputation_score,
+                },
+                "governance_score": {
+                    "final": 0,
+                    # "log_governance_score": log_governance_score,
+                },
+                "onchain_score": {
+                    "badges_minted": badges_minted,
+                    "quest_completed": quest_completed,
+                    "total_badges_reward": payload.total_badges_reward,
+                    "referral_count": referral_count,
+                    "final": onchain_score,
+                    # "log_onchain_score": total_onchain_score,
+                },
+                "identifi_score": final_identifi_score
+                # "log_identifi_score": log_social_score + log_reputation_score + total_onchain_score + log_governance_score
+            }
+        except Exception as e:
+            logger.exception("calculate_identifi_log_err: %s", e)
+            raise 
